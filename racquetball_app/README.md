8d80cd98e7ffb17c8dd46c52d6aa7654542ae30e
RWagner2444 <57501007+RWagner2444@users.noreply.github.com>
2024-12-30 21:29:17 -0600

    initial commit

---
+++ racquetball_app/README.md
@@ -0,0 +1,3 @@
+# racquetball_app
+
+A new Flutter project.
---
+++ racquetball_app/android/app/src/main/kotlin/com/example/racquetball_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.racquetball_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
