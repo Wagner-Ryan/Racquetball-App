8ae36c5a383bc723648adb1f8c02cfe6461bb5c5
RWagner2444 <57501007+RWagner2444@users.noreply.github.com>
2025-02-06 10:41:43 -0600

    home page design

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -9,12 +9,95 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        backgroundColor: Colors.white,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 350,
+                  height: 300,
+                  child: Image.asset('images/logo.jpg'),
+                ),
+                Text(
+                  'RACQUETBALL APP',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height: 50),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  ),
+                  onPressed: user_select,
+                  child: const Text('Select User', style: TextStyle(fontSize: 18))
+                ),
+                SizedBox(height: 2),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  ),
+                  onPressed: record_match,
+                  child: const Text('Record Match', style: TextStyle(fontSize: 18))
+                ),
+                SizedBox(height: 2),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  ),
+                  onPressed: game_history,
+                  child: const Text('Game History', style: TextStyle(fontSize: 18))
+                ),
+                SizedBox(height: 2),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  ),
+                  onPressed: how_to_play,
+                  child: const Text('How To Play', style: TextStyle(fontSize: 18))
+                ),
+                SizedBox(height: 2),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  ),
+                  onPressed: find_new,
+                  child: const Text('Find New', style: TextStyle(fontSize: 18))
+                ),
+              ],
+            ),
+          ),
       ),
     );
   }
+}
+
+void user_select(){
+  print('User Select');
+}
+
+void record_match(){
+  print('Record Match');
+}
+
+void game_history(){
+  print('Game History');
+}
+
+void how_to_play(){
+  print('How to Play');
+}
+
+void find_new(){
+  print('Find New');
 }
